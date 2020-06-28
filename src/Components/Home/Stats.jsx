ed607cbd15083020a5d1772d383286d88d027293
Justin Tzuriel Krisnahadi <justin.tzuriel17@gmail.com>
2020-06-28 23:15:33 +0800

    Added instructions

--- src/Components/Home/Stats.jsx
+++ src/Components/Home/Stats.jsx
@@ -7,9 +7,12 @@ const StatsWrapper = styled.div`
   width: 400px;
   height: 100%;
   text-align: center;
+  justify-content: space-between;
   color: #355428;
   overflow: hidden;
 `;
+
+const MCWrapper = styled.div``;
 
 const StyledSpan = styled.p`
   font-size: 20px;
@@ -23,12 +26,35 @@ const Number = styled.p`
   font-weight: 600;
 `;
 
+const Instructions = styled.p`
+  text-align: left;
+  padding: 20px;
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+const List = styled.ul`
+  padding-left: 20px;
+  font-size: 14px;
+  font-weight: normal;
+`;
+
 class Stats extends Component {
   render() {
     return (
       <StatsWrapper>
-        <StyledSpan>No. of MCs in planner:</StyledSpan>
-        <Number>{this.props.noMcs}</Number>
+        <MCWrapper>
+          <StyledSpan>No. of MCs in planner:</StyledSpan>
+          <Number>{this.props.noMcs}</Number>
+        </MCWrapper>
+        <Instructions>
+          How to use:
+          <List>
+            <li>Select a module from the module bank or from a cell</li>
+            <li>Click on a cell to place the selected module</li>
+            <li>Double-click on a cell to delete the module</li>
+          </List>
+        </Instructions>
       </StatsWrapper>
     );
   }
